d912c0dac55d6caa8df6821b56bf9d1648b1f34a
Simon Hooker <simon@myschoolportal.co.uk>
2019-03-28 11:58:27 +0000

    Adding Gatsby configuration

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  // Customize me!
+  siteMetadata: {
+    company: "Mea Culpa",
+    domain: "https://mea-culpa.net",
+    defaultTitle: "Website Change Monitoring Products and Services",
+    preamble:
+      "We kill things",
+    defaultDescription:
+      "Yaaaaaay",
+    postamble: "Think we can help your project? We'd love to hear from you:",
+    contact: {
+      email: "hello@mea-culpa.net",
+    },
+    menuLinks: [],
+  },
+  pathPrefix: "/tiny-agency",
+  plugins: [
+    "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-remove-serviceworker",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./images/logo.png",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-120162676-1",
+      },
+    },
+  ],
+};
